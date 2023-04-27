d4e0b4704e4a9b7a760d8f7212593825c85c1a3c
OzgeVuralKoca <ozgevuralkoca@gmail.com>
2023-04-28 00:24:16 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(".home", {
+      spaceBetween: 30,
+      hashNavigation: {
+        watchState: true,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
