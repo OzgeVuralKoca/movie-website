a06b5b6116f4d08a6a07c309f025fb5f49dc81cd
OzgeVuralKoca <ozgevuralkoca@gmail.com>
2023-04-28 12:41:53 +0300

    .

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 var swiper = new Swiper(".home", {
       spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
       hashNavigation: {
         watchState: true,
       },
